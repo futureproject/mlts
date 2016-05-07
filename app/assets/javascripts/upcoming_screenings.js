bb7f8e807d53d43735e857f466892b5aa563d349
Josh <joshjacobson14@gmail.com>
2016-05-07 01:15:59 -0400

    Google maps hello world

--- app/assets/javascripts/upcoming_screenings.js
+++ app/assets/javascripts/upcoming_screenings.js
@@ -1,3 +1,14 @@
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: -34.397, lng: 150.644},
+    zoom: 8
+  });
+  $('#map').on('shown', function () {
+    google.maps.event.trigger(map, 'resize');
+    map.setCenter(new google.maps.LatLng(42.3605336, -72.6362989));
+  })
+}
+
 var upcoming_screenings = function() {
 
   // Google spreadsheet as a CMS for Screenings
