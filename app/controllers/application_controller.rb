fdc4379b265deeb3bb33d3eba4fbcbf300cc23b7
Josh <joshjacobson14@gmail.com>
2016-08-17 23:46:15 -0700

    fetch screenings from database

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,35 +2,4 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  helper_method :screenings
-  caches_action :screenings, layout: false, expires_in: 1.hour
-  include Comfy::CmsHelper
-  def screenings
-    screenings_data = @cms_page.children.find_by_slug('screenings').children
-    length = screenings_data.length
-    @screenings = Array.new(length) { Hash.new }
-    screenings_data.each_with_index do |screening, i|
-      save_screening(screening)
-      @screenings[i] = {
-        "city" => cms_block_content('city', screening),
-        "date_and_time" => cms_block_content('date_and_time', screening),
-        "country" => cms_block_content('country', screening),
-        "eventbrite_link" => cms_block_content('eventbrite_link', screening),
-        "venue_name" => cms_block_content('venue_name', screening),
-        "address" => cms_block_content('address', screening)
-      }
-    end
-    @screenings
-  end
-
-  def save_screening(screening)
-    Screening.create({
-      city: cms_block_content('city', screening),
-      screening_time: cms_block_content('date_and_time', screening),
-      country: cms_block_content('country', screening),
-      link: cms_block_content('eventbrite_link', screening),
-      venue_name: cms_block_content('venue_name', screening),
-      street_address: cms_block_content('address', screening)
-    })
-  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  include Comfy::CmsHelper
+  def fetch_cms_screenings
+    cms_screenings = @cms_page.children.find_by_slug('screenings').children
+    cms_screenings.each do |screening|
+      Screening.create({
+        city: cms_block_content('city', screening),
+        screening_time: cms_block_content('date_and_time', screening),
+        country: cms_block_content('country', screening),
+        link: cms_block_content('eventbrite_link', screening),
+        venue_name: cms_block_content('venue_name', screening),
+        street_address: cms_block_content('address', screening)
+      })
+    end
+  end
 end
