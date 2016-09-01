c421b23efdd64299b97650bca2e8c2cc70633c66
Josh <joshjacobson14@gmail.com>
2016-09-01 14:34:29 -0400

    fix page routing

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,9 @@
 class PagesController < ApplicationController
   def show
     @cms_page = Comfy::Cms::Site.first.pages.root
-    render template: 'pages/homepage'
+    render template: "pages/#{params[:page]}"
+  end
+
+  def about
   end
 end
