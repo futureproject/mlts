2f809d27ad929aaab6682dca8ff43825dfa1180b
Josh <joshjacobson14@gmail.com>
2016-09-01 18:26:11 -0400

    render quotes in homepage from db

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,7 @@
 class PagesController < ApplicationController
   def show
     @cms_page = Comfy::Cms::Site.first.pages.root
+    @quotes = Quote.all.each_slice(3).to_a
     render template: "pages/#{params[:page]}"
   end
 
