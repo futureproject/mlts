4f96568b7bed509df3cbb7e4a6759cdfa7efcda9
Josh <joshjacobson14@gmail.com>
2016-09-08 10:57:03 -0400

    render makers on map for all screenings

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,11 @@
 class PagesController < ApplicationController
   def show
     @quotes = Quote.all.each_slice(3).to_a
+    @screenings = Screening.where('screening_time > ?', DateTime.now).sort_by &:screening_time
+    @screenings_hash = Gmaps4rails.build_markers(@screenings) do |screening, marker|
+      marker.lat screening.latitude
+      marker.lng screening.longitude
+    end
     render template: "pages/#{params[:page]}"
   end
 
