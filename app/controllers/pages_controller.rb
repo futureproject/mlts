451c7caefb4fd6eda8821275a70d450fe41dbea4
Josh <joshjacobson14@gmail.com>
2016-09-22 13:30:23 -0400

    implement caching for screenings

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,15 +1,30 @@
 class PagesController < ApplicationController
   def show
     @quotes = Quote.all.each_slice(3).to_a
-    @screenings = Screening.where('screening_time > ?', DateTime.now).sort_by &:screening_time
-    @screenings_hash = Gmaps4rails.build_markers(@screenings) do |screening, marker|
-      marker.lat screening.latitude
-      marker.lng screening.longitude
-      marker.infowindow screening.map_label
-    end
+    @screenings = all_future_screenings
+    @screenings_gmaps_hash = screenings_gmaps_hash
     render template: "pages/#{params[:page]}"
   end
 
   def about
   end
+
+  private
+
+  def all_future_screenings
+    Rails.cache.fetch("screenings", expires_in: 5.minutes) do
+      @screenings = Screening.where('screening_time > ?', DateTime.now).sort_by &:screening_time
+    end
+  end
+
+  def screenings_gmaps_hash
+    Rails.cache.fetch("screenings_gmaps_hash", expires_in: 5.minutes) do
+      Gmaps4rails.build_markers(all_future_screenings) do |screening, marker|
+        marker.lat screening.latitude
+        marker.lng screening.longitude
+        marker.infowindow screening.map_label
+      end
+    end
+  end
+
 end
