b6ad4026bc10dfc2483b82c02a9636402be9142d
Josh <joshjacobson14@gmail.com>
2016-12-29 16:33:55 -0500

    Initial implementation of fragment caching for screenings

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,9 +2,8 @@ class PagesController < ApplicationController
   def show
     @quotes = quotes_hash
     @screenings = all_future_screenings
-    @screenings_gmaps_hash = screenings_gmaps_hash
+    @markers = screenings_gmaps_hash
     render template: "pages/#{params[:page]}"
-    expires_in 15.minutes, :public => true
   end
 
   def about
@@ -19,18 +18,14 @@ class PagesController < ApplicationController
   end
 
   def all_future_screenings
-    Rails.cache.fetch("screenings", expires_in: 12.hours) do
       @screenings = Screening.where('screening_time > ?', DateTime.now).sort_by &:screening_time
-    end
   end
 
   def screenings_gmaps_hash
-    Rails.cache.fetch("screenings_gmaps_hash", expires_in: 12.hours) do
-      Gmaps4rails.build_markers(all_future_screenings) do |screening, marker|
-        marker.lat screening.latitude
-        marker.lng screening.longitude
-        marker.infowindow screening.map_label
-      end
+    Gmaps4rails.build_markers(all_future_screenings) do |screening, marker|
+      marker.lat screening.latitude
+      marker.lng screening.longitude
+      marker.infowindow screening.map_label
     end
   end
 
