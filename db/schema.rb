720f01d1911144acde9f9a27ee0bc542a01e076d
Josh <joshjacobson14@gmail.com>
2017-04-27 13:15:26 -0400

    add state field

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170430042337) do
+ActiveRecord::Schema.define(version: 20170427164730) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -75,6 +75,7 @@ ActiveRecord::Schema.define(version: 20170430042337) do
     t.datetime "updated_at",     null: false
     t.float    "latitude"
     t.float    "longitude"
+    t.string   "state"
   end
 
   create_table "team_members", force: :cascade do |t|
