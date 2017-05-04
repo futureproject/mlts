c8453980c2bd791f78bc28ee6c20ccec6de17a69
Josh <joshjacobson14@gmail.com>
2017-05-04 16:17:47 -0400

    Working local xlsx import

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170428165448) do
+ActiveRecord::Schema.define(version: 20170430042337) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,16 @@ ActiveRecord::Schema.define(version: 20170428165448) do
 
   add_index "admins", ["email"], name: "index_admins_on_email", unique: true, using: :btree
   add_index "admins", ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true, using: :btree
+
+  create_table "change_agents", force: :cascade do |t|
+    t.string   "name"
+    t.string   "title"
+    t.string   "location"
+    t.string   "quote"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string   "link"
+  end
 
   create_table "partners", force: :cascade do |t|
     t.string   "name"
