f96d901a0f7011190538eeaa95000195ac708519
Josh <joshjacobson14@gmail.com>
2017-07-06 22:55:34 -0400

    display change agent names from first&last fields

--- app/models/change_agent.rb
+++ app/models/change_agent.rb
@@ -1,3 +1,10 @@
 class ChangeAgent < ActiveRecord::Base
   validates_length_of :quote, :maximum => 255
+  validates_presence_of :first_name, :last_name, :location, :quote
+
+  default_scope { order(last_name: :asc) }
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
 end
