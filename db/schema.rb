c5af98cb5c85ddb3689105bebc6732ccffdc9e44
Josh <joshjacobson14@gmail.com>
2017-10-09 11:58:50 -0400

    add colors to tracks

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170923022247) do
+ActiveRecord::Schema.define(version: 20171009154109) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -118,6 +118,7 @@ ActiveRecord::Schema.define(version: 20170923022247) do
     t.string   "main_image_content_type"
     t.integer  "main_image_file_size"
     t.datetime "main_image_updated_at"
+    t.string   "color"
   end
 
 end
