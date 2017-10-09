d4716de6566bf84be9f722d946a096142d3c12b2
Josh <joshjacobson14@gmail.com>
2017-10-09 17:14:54 -0400

    iniitial implementation of tabs, css needs refactor

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -10,6 +10,7 @@ class TracksController < ApplicationController
 
   def show
     @track = Track.find(params[:id])
+    @darker_color = darken_color(@track.color, 0.2)
     @location = params[:location]
     if @location == "ND"
       @previous_track = Track.find_by_position(@track.position-1)
@@ -19,4 +20,16 @@ class TracksController < ApplicationController
       @next_track = Track.where.not("targeted_location LIKE ?", "%ND%").find_by_position(@track.position+1)
     end
   end
+
+  private
+  # Amount should be a decimal between 0 and 1. Lower means darker
+  def darken_color(hex_color, amount)
+    hex_color = hex_color.gsub('#','')
+    rgb = hex_color.scan(/../).map {|color| color.hex}
+    rgb[0] = (rgb[0].to_i * amount).round
+    rgb[1] = (rgb[1].to_i * amount).round
+    rgb[2] = (rgb[2].to_i * amount).round
+    "#%02x%02x%02x" % rgb
+  end
+
 end
